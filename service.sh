eacccbb4916cf2d1dff00c2c7a72ab7466b7c829
David Feltell <dave@feltell.net>
2017-12-23 03:00:08 +0000

    Stop dominos container when rebuilding for testing

--- service.sh
+++ service.sh
@@ -5,6 +5,12 @@ fi
 #export site=$(docker inspect lamp | jq -r '.[].NetworkSettings.Networks.bridge.IPAddress')
 echo "Starting bot linked to site $site_host with $daemon"
 #dns=$(nmcli --fields ip4.dns con show "Wired connection 1" | head -1 | tr -s " " | cut -d' ' -f2)
+if [[ "$(docker ps -q -f name=dominos)" != "" ]]; then
+  docker stop dominos
+fi
+if [[ "$(docker ps -a -q -f name=dominos)" != "" ]]; then
+  docker rm dominos
+fi
 docker build -t dominos_voucher_puppet service &&\
 docker run -e site_host -e bot_secret --name dominos $daemon -it \
   --sysctl net.ipv6.conf.all.disable_ipv6=1 --expose 9229 --shm-size 1G --net dominos \
